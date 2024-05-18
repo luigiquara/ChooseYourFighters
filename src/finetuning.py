a4c51024c734448b03465517c1e21342913dc255
Luigi Quarantiello <luigi.quarantiello@phd.unipi.it>
2024-05-18 11:59:34 +0000

    small fixes to make everything work

--- src/finetuning.py
+++ src/finetuning.py
@@ -1,16 +1,19 @@
 import argparse
 from datetime import datetime
+import wandb
 
 from torch import nn
 from torch.optim import Adam
+from torch.utils.data import DataLoader
 
 from loader import Loader 
 from training import Trainer
 
 class BackboneWithClassifier(nn.Module):
     def __init__(self, backbone, n_classes):
+        super().__init__()
         self.backbone = backbone
-        self.classifier = nn.Linear(in_features, n_classes)
+        self.classifier = nn.LazyLinear(n_classes)
 
     def forward(self, x):
         return self.classifier(self.backbone(x))
@@ -29,19 +32,28 @@ def run(params):
     l = Loader()
 
     # load the specified model, or all of them if not specified
+    print('Loading models')
     if params.models_name:
-        s, n, m, t = l.load_model(params.models_name, params.device)
-        sources = [s]
-        m_names = [n]
-        models = [m]
-        transforms = [t]
-    else: sources, m_names, models, transforms = l.load_all_model(params.device)
+        sources, m_names, models, transforms = [], [], [], []
+        for model_name in params.models_name:
+            print(model_name)
+            s, n, m, t = l.load_model(model_name, params.device)
+            sources.append(s)
+            m_names.append(n)
+            models.append(m)
+            transforms.append(t)
+    else: sources, m_names, models, transforms = l.load_all_models(params.device)
 
     # load the specified dataset, or all of them if not specified
+    print('Loading datasets')
     if params.datasets_name:
-        n, d = l.load_dataset(params.datasets_name, params.dataset_args)
-        d_names = [n]
-        datasets = [d]
+        d_names, datasets = [], []
+        for dataset_name in params.datasets_name:
+            if params.dataset_args: n, d = l.load_dataset(dataset_name, params.dataset_args)
+            else: n, d = l.load_dataset(dataset_name)
+
+        d_names.append(n)
+        datasets.append(d)
     else: d_names, datasets = l.load_all_datasets()
 
     # for every combination of loaded models and datasets
@@ -56,9 +68,9 @@ def run(params):
                   config = {
                     'model': m_name,
                     'dataset': d_name,
-                    'max_epochs': param.max_epochs,
-                    'batch_size': param.batch_size,
-                    'lr': param.lr
+                    'max_epochs': params.max_epochs,
+                    'batch_size': params.batch_size,
+                    'lr': params.lr
                 })
                 save_path = params.save_root + run.name
             else: save_path = params.save_root + datetime.now().strftime('%H%M%S')
@@ -68,21 +80,23 @@ def run(params):
             val_loader = DataLoader(dataset['val'], batch_size=params.batch_size, collate_fn=collate_fn)
             num_classes = len(dataset['train'].unique('label'))
 
+            print('Finetuning...')
             finetune(model, train_loader, val_loader, num_classes, params.max_epochs, params.lr, save_path, params.device, params.log)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', nargs='*', dest='models_name', type=str)
     parser.add_argument('--dataset', nargs='*', dest='datasets_name', type=str)
-    parser.add_argument('--dataset_args', type=str)
+    parser.add_argument('--dataset_args', type=str, default=None)
 
     parser.add_argument('--max_epochs', type=int, default=50)
     parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--batch_size', type=int, default=32)
 
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--log', action='store_true')
     parser.add_argument('--no-log', action='store_false', dest='log')
-    parser.add_argument('--save_root', type=str, default='./fighters/')
+    parser.add_argument('--save_root', type=str, default='/disk4/lquarantiello/chooseyourfighters/fighters/')
 
     params = parser.parse_args()
     run(params)
